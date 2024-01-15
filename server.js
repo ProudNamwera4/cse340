23b1ebd4eae6fb2c93d5ac20adcb2ad95ab2ae0f
ProudNamwera4 <proudnamwera4@gmail.com>
2024-01-15 13:21:05 +0200

    week 1 learning acticities

--- server.js
+++ server.js
@@ -6,14 +6,29 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
- * Routes
+ * View Engine and Templates
  *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
+ * Routes
+ *************************/app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
+
+// Index route
+app.get("/", function(req,res){
+  res.render("index", {title:"Home"})
+})
 
 /* ***********************
  * Local Server Information
