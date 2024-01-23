3f7256fa7856fa3c41dbad5b0851488bda86411d
ProudNamwera4 <proudnamwera4@gmail.com>
2024-01-23 17:23:48 +0200

    database NODE_ENV == "production")

--- database/index.js
+++ database/index.js
@@ -7,7 +7,7 @@ require("dotenv").config()
  * If - else will make determination which to use
  * *************** */
 let pool
-if (process.env.NODE_ENV == "development") {
+if (process.env.NODE_ENV == "production") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
